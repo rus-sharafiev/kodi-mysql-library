2b9ce5f54e8e0b2465a48e0a15996035530b964e
Rus Sharafiev <srr@live.ru>
2023-06-27 22:37:54 +0300

    Add show/hide bottom sheet on slug

--- src/components/BottomSheet.tsx
+++ src/components/BottomSheet.tsx
@@ -1,32 +1,27 @@
-import { useEffect } from "react"
-import { useAppDispatch, useAppSelector } from "../store"
-import { hideBottomSheet, resetBottomSheetData } from "../store/reducers/bottomSheetSlice"
+import { useEffect, useRef } from "react"
+import { useNavigate } from "react-router-dom"
+import { useAppSelector } from "../store"
 import Movie from "./Movie"
 
 // --------------------------------------------------------------------------------
 
 export const BottomSheet: React.FC = () => {
     const { movie, tv, season, isVisible } = useAppSelector(state => state.bottomSheet)
-    const dispatch = useAppDispatch()
+    const nav = useNavigate()
+    const ref = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        if (isVisible)
-            document.body.classList.add('no-scroll')
-        else
-            setTimeout(() => {
-                document.body.removeAttribute('class')
-                dispatch(resetBottomSheetData())
-            }, 300)
-
+        document.body.classList.toggle('no-scroll', isVisible)
+        isVisible && ref.current && ref.current.scrollTo(0, 0)
     }, [isVisible])
 
     return (
-        <div className={isVisible ? 'bottom-sheet' : 'bottom-sheet hidden'}>
+        <div className={isVisible ? 'bottom-sheet' : 'bottom-sheet hidden'} ref={ref}>
 
-            <md-standard-icon-button onClick={() => dispatch(hideBottomSheet())}>
+            <md-standard-icon-button onClick={() => nav(-1)}>
                 <md-icon>close</md-icon>
             </md-standard-icon-button>
-            {movie && <Movie data={movie} />}
+            {!!movie && <Movie data={movie} />}
         </div>
     )
 }
--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -1,20 +1,32 @@
+import { useNavigate, useParams } from "react-router-dom"
 import { useAppDispatch } from "../store"
 import { useGetMoviesQuery } from "../store/api/api"
-import { setBottomSheetData } from "../store/reducers/bottomSheetSlice"
+import { hideBottomSheet, setBottomSheetData } from "../store/reducers/bottomSheetSlice"
 import proxyImage from "../utils/proxyImage"
 import Rating from "./Rating"
+import { useEffect, useRef } from "react"
 
 // --------------------------------------------------------------------------------
 
 export const Movies: React.FC = () => {
-    const { data: movies, isFetching } = useGetMoviesQuery()
+    const { data: movies } = useGetMoviesQuery()
     const dispatch = useAppDispatch()
+    const { slug } = useParams()
+    const nav = useNavigate()
+    const ref = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if (!slug) dispatch(hideBottomSheet())
+    }, [slug])
 
     return (
-        <div className="cards">
+        <div className="cards" ref={ref}>
             {movies && movies.map(movie =>
                 <div className="card" key={`movie-${movie.id}`}>
-                    <div className="poster" onClick={() => dispatch(setBottomSheetData({ movie }))}>
+                    <div className="poster" onClick={() => {
+                        dispatch(setBottomSheetData({ movie }))
+                        nav(`/movies/${movie.originalTitle.replaceAll(' ', '_')}`)
+                    }}>
                         <md-ripple></md-ripple>
                         <md-elevation></md-elevation>
                         <img src={proxyImage(movie.art.poster.replace('original', 'w300'))} />
@@ -23,8 +35,7 @@ export const Movies: React.FC = () => {
                         <div className="title body-small">{movie.title}</div>
                         <Rating rating={movie.rating} />
                     </div>
-                </div>
-            )}
+                </div>)}
         </div>
     )
 }
