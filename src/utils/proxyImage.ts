395d6f67b9e0ab8c628990d9c472fe8a6a9457c1
Rus Sharafiev <srr@live.ru>
2023-07-01 18:58:11 +0300

    Fix proxy image function to return string

--- src/utils/proxyImage.ts
+++ src/utils/proxyImage.ts
@@ -1,8 +1,8 @@
 export const proxyImage = (url: string) => {
-    if (!url) return;
-    let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+    if (!url) return ''
+    let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url='
 
-    return (googleProxyURL + encodeURIComponent(url));
+    return (googleProxyURL + encodeURIComponent(url))
 }
 
 export default proxyImage
